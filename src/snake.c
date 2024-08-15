3260e85b015ddc1dd1b16430fdee6aabc649f0c4
egreen45 <ethanmgreen45@gmail.com>
2024-08-15 14:27:35 -0400

    switched from surface based to renderer

--- src/snake.c
+++ src/snake.c
@@ -1,40 +1,54 @@
 #include "snake.h"
 
 const int SCREEN_WIDTH = 640; 
-const int SCREEN_HEIGHT = 480; 
+const int SCREEN_HEIGHT = 640; 
 
 int main(void){
   SDL_Window* window = NULL; 
-  SDL_Surface* screenSurface = NULL; 
+  SDL_Renderer* renderer = NULL; 
 
-  if (SDL_Init(SDL_INIT_VIDEO) < 0) 
+  if (SDL_Init(SDL_INIT_VIDEO) < 0) {
     printf("SDL could not initialize! SDL_Error: %s\n", SDL_GetError()); 
-  else {
-    window = SDL_CreateWindow("SDL Tutorial",
-                              SDL_WINDOWPOS_UNDEFINED,
-                              SDL_WINDOWPOS_UNDEFINED, 
-                              SCREEN_WIDTH, 
-                              SCREEN_HEIGHT, 
-                              SDL_WINDOW_SHOWN); 
+    goto _return; 
+  } 
+ 
+  window = SDL_CreateWindow("Snake Game", SDL_WINDOWPOS_CENTERED, 
+                                          SDL_WINDOWPOS_CENTERED, 
+                                          SCREEN_WIDTH, 
+                                          SCREEN_HEIGHT, 
+                                          0); 
+
+  if (window == NULL) {
+    printf("SDL could not create window! SDL_Error: %s\n", SDL_GetError()); 
+    goto _return;   
+  }
+
+  renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED); 
+
+  if (renderer == NULL) {
+    printf("SDL could not create renderer! SDL_Error: %s\n", SDL_GetError()); 
+    goto _return; 
+  }
   
-    if (window == NULL) 
-      printf("Window could not be created! SDL_Error: %s\n", SDL_GetError()); 
-    else {
-      screenSurface = SDL_GetWindowSurface(window); 
-      SDL_FillRect(screenSurface, NULL, SDL_MapRGB(screenSurface -> format,   
-                                                    0xFF,  0xFF,  0xFF)); 
-      SDL_UpdateWindowSurface(window); 
-      SDL_Event e; 
-      bool quit = false; 
-      while (!quit) {
-        while(SDL_PollEvent(&e)) {
-          if (e.type == SDL_QUIT) quit = true; 
-        }
+  bool quit = false; 
+  while(!quit) {
+    SDL_Event event; 
+
+    while(SDL_PollEvent(&event)) {
+      switch(event.type) {
+        case SDL_QUIT: 
+          quit = true; 
+          break;   
       }
     }
+    SDL_SetRenderDrawColor(renderer, 0x8, 0x8, 0x8, SDL_ALPHA_OPAQUE); 
+    SDL_RenderClear(renderer); 
+    SDL_RenderPresent(renderer);  
   }
-  SDL_DestroyWindow(window); 
-  SDL_Quit(); 
+  
+_return: 
+  if (window != NULL) SDL_DestroyWindow(window); 
+  if (renderer != NULL) SDL_DestroyRenderer(renderer); 
 
   return 0; 
 }
